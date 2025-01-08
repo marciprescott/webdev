df30eeb8d69197d5efc686433070631a7a57f0ed
Marci Prescott <mjprescott@me.com>
2025-01-08 16:15:08 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import e from "express";
 
 const app = express();
 const port = 3000;
@@ -9,8 +10,8 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "secrets",
-  password: "123456",
-  port: 5432,
+  password: "winston",
+  port: 5433,
 });
 db.connect();
 
@@ -33,51 +34,26 @@ app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
 
-  try {
-    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
+  const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
 
-    if (checkResult.rows.length > 0) {
-      res.send("Email already exists. Try logging in.");
-    } else {
-      const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
-        [email, password]
-      );
-      console.log(result);
-      res.render("secrets.ejs");
-    }
-  } catch (err) {
-    console.log(err);
+  if (checkResult.rows.length > 0) {
+    res.send("User already exists");
+    return;
+  } else {
+    const result = await db.query(
+      "INSERT INTO users(email, password) VALUES($1, $2)",
+      [email, password]
+    );
   }
+  console.log(result);
+  res.render("secrets.ejs");
 });
-
 app.post("/login", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
-
-  try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      const storedPassword = user.password;
-
-      if (password === storedPassword) {
-        res.render("secrets.ejs");
-      } else {
-        res.send("Incorrect Password");
-      }
-    } else {
-      res.send("User not found");
-    }
-  } catch (err) {
-    console.log(err);
-  }
 });
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
